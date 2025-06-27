9ca6cbc8438b92041ea1e8200fd4155d5fb4cb1e
Emmanuel <alemmanuel0412@gmail.com>
2025-06-27 16:55:35 -0500

    Seventh Launch

--- core/vector.py
+++ core/vector.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 CHROMA_DIR = "./vectorstore"
 _vectordb = None
